bcdd47adabbdde08611cc8b510dc21485329dfa8
Atta ul saboor <Atta-ul.saboor@mohawkcollege.ca>
2023-10-04 13:43:53 -0400

    securely store source code for Lab 1 to an online source code repository

---
+++ Models/Employer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Models
+{
+    public class Employer
+    {
+        [Required]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Website")]
+        [DataType(DataType.Url)]
+        public string Website { get; set; }
+
+        [Display(Name = "Incorporated Date")]
+        [DataType(DataType.Date)]
+        public DateTime? IncorporatedDate { get; set; }
+    }
+}
